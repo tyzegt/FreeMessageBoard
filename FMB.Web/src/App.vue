273a4e1f920268f4554ceaec3c0ac11a1358bec2
duzenko <anton.duzenko@gmail.com>
2023-08-05 20:21:33 +0300

    added frontend stub (FMB.Web)

---
+++ FMB.Web/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <router-link to="/top">Лучшее</router-link> |
+    <router-link to="/trending">Горячее</router-link> |
+    <router-link to="/new">Свежее</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
